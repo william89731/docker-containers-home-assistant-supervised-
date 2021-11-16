7b6482305185ef1205d35dae0623be8352455111
Fabrizio Mancini <fabrizio@darth.lan>
2021-11-16 19:39:20 +0100

    caricati i profili apparmor che non ci stavano

--- install.sh
+++ install.sh
@@ -2,12 +2,19 @@
 
 #da qui si possono impostare tutti i parametri dei files e delle cartelle
 BASE_DIR=/opt/hassio
-SETUP_DIR=$BASE_DIR/setup
+SETUP_DIR=$BASE_DIR/scripts
 APPARMOR_SETUP=$SETUP_DIR/apparmor_setup.sh
 HASSIO_APPARMOR=$SETUP_DIR/hassio-apparmor
 HASSIO_JSON=$SETUP_DIR/hassio.json
 #trovo quale distro è
 DISTRO=$(cat /etc/issue|awk '{print $1}'|tr '[:upper:]' '[:lower:]')
+COMPOSE_DIR=$BASE_DIR
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Questo script deve essere eseguito come root" 
+   exit 1
+fi
 
 echo "
 	 ##         ######   ###   ##  ##    ##  ##    ## 
@@ -22,12 +29,13 @@ echo "
 	 ##           ##     ##  :###  ##    ##   ##::##  
 	 ########   ######   ##   ###  :######:  :##  ##: 
 	 ########   ######   ##   ###   :####:   ##    ## "
-sleep 3	  
+sleep 1	  
 echo "Benvenuto,figlio della perdizione "
+#questo codice non funziona su Debian
 printf "\U$(printf %08x 128520)\n"
-sleep 3
+sleep 1
 echo "stai per installare dei containers docker di home assistant supervised !"
-sleep 7
+sleep 2
 echo "cominciamo!"
 
 #aggiungo i repo nel caso di ubuntu
@@ -81,6 +89,96 @@ mkdir -p "\${PROFILES_DIR}"
 mkdir -p "\${CACHE_DIR}"
 mkdir -p "\${REMOVE_DIR}"
 
+cat << NEF > "\${PROFILES_DIR}/hassio-supervisor"
+#include <tunables/global>
+
+profile hassio-supervisor flags=(attach_disconnected,mediate_deleted) {
+  #include <abstractions/base>
+  #include <abstractions/python>
+
+  network,
+  deny network raw,
+
+  signal (send) set=(kill,term,int,hup,cont),
+
+  capability net_admin,
+  capability net_bind_service,
+  capability dac_read_search,
+  capability dac_override,
+
+  /bin/** ix,
+  /usr/bin/** ix,
+  /bin/udevadm Ux,
+  /sbin/udevd Ux,
+  /usr/local/bin/python* ix,
+  /usr/bin/git cx,
+  /usr/bin/gdbus cx,
+  /usr/lib/bashio/** ix,
+  /etc/s6/** ix,
+  /run/s6/** ix,
+  /etc/services.d/** rwix,
+  /etc/cont-init.d/** rwix,
+  /etc/cont-finish.d/** rwix,
+
+  deny /proc/** wl,
+  deny /root/** wl,
+  deny /sys/** wl,
+
+  / r,
+  /** r,
+  /tmp/** rw,
+  /data/** rw,
+  /os/** rw,
+  /run/** rwk,
+  /dev/tty rw,
+  /etc/resolv.conf rw,
+  /run/docker.sock rw,
+
+  /usr/local/lib/** mr,
+
+  profile /usr/bin/gdbus flags=(attach_disconnected,mediate_deleted) {
+    #include <abstractions/base>
+    #include <abstractions/dbus>
+
+    signal (receive) set=(int),
+    unix (send, receive) type=stream,
+
+    capability sys_nice,
+
+    /** r,
+    /lib/* mr,
+    /usr/bin/gdbus mr,
+    /usr/local/lib/** mr,
+
+    /run/dbus/system_bus_socket rw,
+  }
+
+  profile /usr/bin/git flags=(attach_disconnected,mediate_deleted) {
+    #include <abstractions/base>
+
+    network,
+    deny network raw,
+
+    signal (receive) set=(term),
+
+    /bin/busybox ix,
+    /usr/bin/git mr,
+    /usr/libexec/git-core/* ix,
+
+    deny /data/homeassistant rw,
+    deny /data/ssl rw,
+
+    /** r,
+    /lib/* mr,
+    /data/addons/** lrw,
+    /usr/local/lib/** mr,
+
+    capability dac_override,
+  }
+}
+
+NEF
+
 # Load/Update profili
 for profile in "\${PROFILES_DIR}"/*; do
     if [ ! -f "\${profile}" ]; then
@@ -110,17 +208,30 @@ done
 FOE
 chmod +x $HASSIO_APPARMOR
 #repeated on purpose
-#$HASSIO_APPARMOR
-#$HASSIO_APPARMOR
+$HASSIO_APPARMOR
+$HASSIO_APPARMOR
 FNE
 chmod +x $APPARMOR_SETUP
+$APPARMOR_SETUP
 
-#già fatto prima...
-#mkdir -p $SETUP_DIR
-#cd $SETUP_DIR
-cat << EOF >docker-compose.yaml
+if [[ -d "$COMPOSE_DIR/docker-compose.yml" ]]; then
+  COMPOSE_FILE=$COMPOSE_DIR/docker-compose.yml
+fi
+
+if [[ -d "$COMPOSE_DIR/docker-compose.yaml" ]]; then
+  COMPOSE_FILE=$COMPOSE_DIR/docker-compose.yaml
+fi
+if [[ ! -d "$COMPOSE_FILE" ]]; then
+  #non esiste nessuna delle due versioni del compose file pertanto lo creiamo
+  COMPOSE_FILE=$COMPOSE_DIR/docker-compose.yaml  
+  cat << EOF > $COMPOSE_FILE
 version: '3.8'
 services:
+EOF
+fi   
+
+#sia che esista il compose o che lo abbia creato io, finisco di inserire i valori.
+cat << EOF >> $COMPOSE_FILE
   hassio_supervisor:
     container_name: hassio_supervisor
     image: "homeassistant/amd64-hassio-supervisor"
@@ -148,7 +259,7 @@ services:
 
     security_opt:
       - seccomp:unconfined
-     # - apparmor:hassio-supervisor
+      - apparmor:hassio-supervisor
 
     environment:
       - SUPERVISOR_SHARE=$BASE_DIR
@@ -158,7 +269,7 @@ services:
     ports:
       - "8124:8123"  
 EOF
-docker-compose up -d 
+#docker-compose up -d 
 
 sleep 5
 
