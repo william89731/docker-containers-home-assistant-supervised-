ac972b8ce1aea0ed3f0ae560eefcb592ec373b73
Fabrizio Mancini <mr.file@gmail.com>
2021-11-17 17:22:29 +0100

    Inserite la scelta delle archietture su install.sh e modificato anche uninstall

--- install.sh
+++ install.sh
@@ -1,19 +1,111 @@
 #!/bin/bash
 
+declare -a MISSING_PACKAGES
+
+function info { echo -e "\e[32m[info] $*\e[39m"; }
+function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
+function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
+
+if [[ $EUID -ne 0 ]]; then
+   error "Questo script deve essere eseguito come root" 
+   exit 1
+fi
+#parametri per la scelta del repo e dell'architettura
+ARCH=$(uname -m)
+IP_ADDRESS=$(hostname -I | awk '{ print $1 }')
+
+# Parse command line parameters
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+
+    case $arg in
+        -m|--machine)
+            MACHINE=$2
+            shift
+            ;;
+        -d|--data-share)
+            DATA_SHARE=$2
+            shift
+            ;;
+        -p|--prefix)
+            BASE_DIR=$2
+            shift
+            ;;
+        -s|--scriptdir)
+            SCRIPT_DIR=$2
+            shift
+            ;;
+        *)
+            error "Unrecognized option $1"
+            ;;
+    esac
+    shift
+done
+
+BASE_DIR=${BASE_DIR:-/opt/hassio}
+SCRIPT_DIR=${SCRIPT_DIR:-$BASE_DIR/scripts}
+DATA_SHARE=${DATA_SHARE:-$BASE_DIR}
+DOCKER_REPO=homeassistant
+
+URL_VERSION_HOST="version.home-assistant.io"
+URL_VERSION="https://version.home-assistant.io/stable.json"
+URL_APPARMOR_PROFILE="https://version.home-assistant.io/apparmor.txt"
+
 #da qui si possono impostare tutti i parametri dei files e delle cartelle
-BASE_DIR=/opt/hassio
-SETUP_DIR=$BASE_DIR/scripts
-APPARMOR_SETUP=$SETUP_DIR/apparmor_setup.sh
-HASSIO_APPARMOR=$SETUP_DIR/hassio-apparmor
-HASSIO_JSON=$SETUP_DIR/hassio.json
+APPARMOR_SETUP=$SCRIPT_DIR/apparmor_setup.sh
+HASSIO_APPARMOR=$SCRIPT_DIR/hassio-apparmor
+HASSIO_JSON=$SCRIPT_DIR/hassio.json
 #trovo quale distro è
 DISTRO=$(cat /etc/issue|awk '{print $1}'|tr '[:upper:]' '[:lower:]')
 COMPOSE_DIR=$BASE_DIR
+TIMEOUT=1
 
+#controllo la connessione di rete
+while ! ping -c 1 -W 1 ${URL_VERSION_HOST}; do
+    info "In attesa di ${URL_VERSION_HOST} - l'interfaccia di rete potrebbe esseere inattiva..."
+    sleep 2
+done
 
-if [[ $EUID -ne 0 ]]; then
-   echo "Questo script deve essere eseguito come root" 
-   exit 1
+# Genera le info HW
+case $ARCH in
+    "i386" | "i686")
+        MACHINE=${MACHINE:=qemux86}
+        HASSIO_DOCKER="$DOCKER_REPO/i386-hassio-supervisor"
+    ;;
+    "x86_64")
+        MACHINE=${MACHINE:=qemux86-64}
+        HASSIO_DOCKER="$DOCKER_REPO/amd64-hassio-supervisor"
+    ;;
+    "arm" |"armv6l")
+        if [ -z $MACHINE ]; then
+            error "Per favore imposta il tipo di macchina (-m) per $ARCH"
+            info "Tipo di macchina: intel-nuc / odroid-c2 / odroid-n2 / odroid-xu / qemuarm / qemuarm-64 / qemux86 / qemux86-64 / raspberrypi / raspberrypi2 / raspberrypi3 / raspberrypi4 / raspberrypi3-64 / raspberrypi4-64 / tinker"
+        fi
+        HASSIO_DOCKER="$DOCKER_REPO/armhf-hassio-supervisor"
+    ;;
+    "armv7l")
+        if [ -z $MACHINE ]; then
+            error "Per favore imposta il tipo di macchina (-m) per $ARCH"
+            info "Tipo di macchina: intel-nuc / odroid-c2 / odroid-n2 / odroid-xu / qemuarm / qemuarm-64 / qemux86 / qemux86-64 / raspberrypi / raspberrypi2 / raspberrypi3 / raspberrypi4 / raspberrypi3-64 / raspberrypi4-64 / tinker"
+        fi
+        HASSIO_DOCKER="$DOCKER_REPO/armv7-hassio-supervisor"
+    ;;
+    "aarch64")
+        if [ -z $MACHINE ]; then
+            error "Per favore imposta il tipo di macchina (-m) per $ARCH"
+            info "Tipo di macchina: intel-nuc / odroid-c2 / odroid-n2 / odroid-xu / qemuarm / qemuarm-64 / qemux86 / qemux86-64 / raspberrypi / raspberrypi2 / raspberrypi3 / raspberrypi4 / raspberrypi3-64 / raspberrypi4-64 / tinker"
+        fi
+        HASSIO_DOCKER="$DOCKER_REPO/aarch64-hassio-supervisor"
+    ;;
+    *)
+        error "$ARCH sconosciuta!"
+        info "ARCH puo' essere: i386 - i686 - x86_64 - arm - armv6l - armv7l - aarch64"
+    ;;
+esac
+
+if [[ ! "${MACHINE}" =~ ^(generic-x86-64|odroid-c2|odroid-n2|odroid-xu|qemuarm|qemuarm-64|qemux86|qemux86-64|raspberrypi|raspberrypi2|raspberrypi3|raspberrypi4|raspberrypi3-64|raspberrypi4-64!tinker|khadas-vim3)$ ]]; then
+    error "Tipo di macchina sconosciuta: ${MACHINE}!"
+    info "Tipo di macchina: generic-x86-64, odroid-c2, odroid-n2, odroid-xu, qemuarm, qemuarm-64, qemux86, qemux86-64, raspberrypi, raspberrypi2, raspberrypi3, raspberrypi4, raspberrypi3-64, raspberrypi4-64, tinker, khadas-vim3"
 fi
 
 echo "
@@ -29,39 +121,60 @@ echo "
 	 ##           ##     ##  :###  ##    ##   ##::##  
 	 ########   ######   ##   ###  :######:  :##  ##: 
 	 ########   ######   ##   ###   :####:   ##    ## "
-sleep 1	  
-echo "Benvenuto,figlio della perdizione "
+sleep $TIMEOUT
+info "Benvenuto,figlio della perdizione "
 #questo codice non funziona su Debian
 printf "\U$(printf %08x 128520)\n"
-sleep 1
-echo "stai per installare dei containers docker di home assistant supervised !"
-sleep 2
-echo "cominciamo!"
+sleep $TIMEOUT
+warn "stai per installare dei containers docker di home assistant supervised !"
+sleep $TIMEOUT
+info "cominciamo!"
 
 #aggiungo i repo nel caso di ubuntu
 if [[ "${DISTRO}" =~ ^(ubuntu)$ ]]; then
   add-apt-repository universe
 fi
 
-echo "Installo i pacchetti necessari..."
+# controlla pacchetti mancanti
+#command -v systemctl > /dev/null 2>&1 || MISSING_PACKAGES+=("systemd")
+command -v nmcli > /dev/null 2>&1 || MISSING_PACKAGES+=("network-manager")
+command -v apparmor_parser > /dev/null 2>&1 || MISSING_PACKAGES+=("apparmor")
+command -v docker > /dev/null 2>&1 || MISSING_PACKAGES+=("docker")
+command -v jq > /dev/null 2>&1 || MISSING_PACKAGES+=("jq")
+command -v curl > /dev/null 2>&1 || MISSING_PACKAGES+=("curl")
+command -v dbus-daemon > /dev/null 2>&1 || MISSING_PACKAGES+=("dbus")
+
+info "Installo i pacchetti necessari..."
 apt-get update
-apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat software-properties-common bluez bluetooth libbluetooth-dev 
-echo "Disabilito ModemManager"
+apt-get install -y apparmor-utils avahi-daemon ca-certificates curl dbus jq network-manager socat software-properties-common bluez bluetooth libbluetooth-dev
+#apt-transport-https  
+info "Disabilito ModemManager"
 systemctl disable ModemManager
 apt-get purge -y modemmanager
 apt autoremove -y
 
+#creo le cartelle se non già presenti
+if [ ! -d "$SCRIPT_DIR" ]; then
+    mkdir -p "$SCRIPT_DIR"
+fi
+if [ ! -d "$DATA_SHARE" ]; then
+    mkdir -p "$DATA_SHARE"
+fi
 
-mkdir -p $SETUP_DIR
-cd $SETUP_DIR
+HASSIO_VERSION=$(curl -s $URL_VERSION | jq -e -r '.supervisor')
+
+cd $SCRIPT_DIR
 
 #rendiamo il file parametrico - ho tolto le referenze a /opt/hassio e sostituito con $BASE_DIR
+info "creo il file $APPARMOR_SETUP"
+info "creo il file $HASSIO_JSON"
+info "creo il file $HASSIO_APPARMOR"
 cat << FNE > $APPARMOR_SETUP
 cat << EOF > $HASSIO_JSON
 {
-    "supervisor": "homeassistant/amd64-hassio-supervisor",
-    "machine": "qemux86-64",
-    "data": "$BASE_DIR"
+    "supervisor": "${HASSIO_DOCKER}",
+    "machine": "${MACHINE}",
+    "data": "${DATA_SHARE}"
 }
 EOF
 
@@ -69,11 +182,8 @@ cat << 'FOE' > $HASSIO_APPARMOR
 #!/usr/bin/env bash
 set -e
 
-# carica config
-CONFIG_FILE=$HASSIO_JSON
-
 # leggi config
-DATA="\$(jq --raw-output '.data // "/usr/share/hassio"' \${CONFIG_FILE})"
+DATA="\$(jq --raw-output '.data // "/usr/share/hassio"' ${HASSIO_JSON})"
 PROFILES_DIR="\${DATA}/apparmor"
 CACHE_DIR="\${PROFILES_DIR}/cache"
 REMOVE_DIR="\${PROFILES_DIR}/remove"
@@ -89,95 +199,7 @@ mkdir -p "\${PROFILES_DIR}"
 mkdir -p "\${CACHE_DIR}"
 mkdir -p "\${REMOVE_DIR}"
 
-cat << NEF > "\${PROFILES_DIR}/hassio-supervisor"
-#include <tunables/global>
-
-profile hassio-supervisor flags=(attach_disconnected,mediate_deleted) {
-  #include <abstractions/base>
-  #include <abstractions/python>
-
-  network,
-  deny network raw,
-
-  signal (send) set=(kill,term,int,hup,cont),
-
-  capability net_admin,
-  capability net_bind_service,
-  capability dac_read_search,
-  capability dac_override,
-
-  /bin/** ix,
-  /usr/bin/** ix,
-  /bin/udevadm Ux,
-  /sbin/udevd Ux,
-  /usr/local/bin/python* ix,
-  /usr/bin/git cx,
-  /usr/bin/gdbus cx,
-  /usr/lib/bashio/** ix,
-  /etc/s6/** ix,
-  /run/s6/** ix,
-  /etc/services.d/** rwix,
-  /etc/cont-init.d/** rwix,
-  /etc/cont-finish.d/** rwix,
-
-  deny /proc/** wl,
-  deny /root/** wl,
-  deny /sys/** wl,
-
-  / r,
-  /** r,
-  /tmp/** rw,
-  /data/** rw,
-  /os/** rw,
-  /run/** rwk,
-  /dev/tty rw,
-  /etc/resolv.conf rw,
-  /run/docker.sock rw,
-
-  /usr/local/lib/** mr,
-
-  profile /usr/bin/gdbus flags=(attach_disconnected,mediate_deleted) {
-    #include <abstractions/base>
-    #include <abstractions/dbus>
-
-    signal (receive) set=(int),
-    unix (send, receive) type=stream,
-
-    capability sys_nice,
-
-    /** r,
-    /lib/* mr,
-    /usr/bin/gdbus mr,
-    /usr/local/lib/** mr,
-
-    /run/dbus/system_bus_socket rw,
-  }
-
-  profile /usr/bin/git flags=(attach_disconnected,mediate_deleted) {
-    #include <abstractions/base>
-
-    network,
-    deny network raw,
-
-    signal (receive) set=(term),
-
-    /bin/busybox ix,
-    /usr/bin/git mr,
-    /usr/libexec/git-core/* ix,
-
-    deny /data/homeassistant rw,
-    deny /data/ssl rw,
-
-    /** r,
-    /lib/* mr,
-    /data/addons/** lrw,
-    /usr/local/lib/** mr,
-
-    capability dac_override,
-  }
-}
-
-NEF
+curl -sL ${URL_APPARMOR_PROFILE} > "\${PROFILES_DIR}/hassio-supervisor"
 
 # Load/Update profili
 for profile in "\${PROFILES_DIR}"/*; do
@@ -185,13 +207,13 @@ for profile in "\${PROFILES_DIR}"/*; do
         continue
     fi
 
-    # Load Profile
+    # Carica i profili apparmor
     if ! apparmor_parser -r -W -L "\${CACHE_DIR}" "\${profile}"; then
-        echo "[Error]: Can't load profile \${profile}"
+        echo "[Errore]: Non riesco a caricare il profilo \${profile}"
     fi
 done
 
-# Cleanup old profili
+# Pulisci i vecchi profili
 for profile in "\${REMOVE_DIR}"/*; do
     if [ ! -f "\${profile}" ]; then
         continue
@@ -203,7 +225,7 @@ for profile in "\${REMOVE_DIR}"/*; do
             continue
         fi
     fi
-    echo "[Error]: Can't remove profile \${profile}"
+    echo "[Error]: Non riesco a rimuovere il profilo \${profile}"
 done
 FOE
 chmod +x $HASSIO_APPARMOR
@@ -214,14 +236,14 @@ FNE
 chmod +x $APPARMOR_SETUP
 $APPARMOR_SETUP
 
-if [[ -d "$COMPOSE_DIR/docker-compose.yml" ]]; then
+if [[ -f "$COMPOSE_DIR/docker-compose.yml" ]]; then
   COMPOSE_FILE=$COMPOSE_DIR/docker-compose.yml
 fi
 
-if [[ -d "$COMPOSE_DIR/docker-compose.yaml" ]]; then
+if [[ -f "$COMPOSE_DIR/docker-compose.yaml" ]]; then
   COMPOSE_FILE=$COMPOSE_DIR/docker-compose.yaml
 fi
-if [[ ! -d "$COMPOSE_FILE" ]]; then
+if [[ ! -f "$COMPOSE_FILE" ]]; then
   #non esiste nessuna delle due versioni del compose file pertanto lo creiamo
   COMPOSE_FILE=$COMPOSE_DIR/docker-compose.yaml  
   cat << EOF > $COMPOSE_FILE
@@ -269,7 +291,7 @@ cat << EOF >> $COMPOSE_FILE
     ports:
       - "8124:8123"  
 EOF
-#docker-compose up -d 
+docker-compose up -d 
 
 sleep 5
 
@@ -286,5 +308,16 @@ echo "
                                                                         
 sleep 3                                                                        
 
-echo "fine installazione. BUON DIVERTIMENTO"
+info "Memorizzo i percorsi per la disinstallazione"
+#è corretto memorizzarli qui?
+cat << EOF > /root/.ha_uninstall
+BASE_DIR=$BASE_DIR
+SCRIPT_DIR=$SCRIPT_DIR
+DATA_SHARE=$DATA_SHARE
+COMPOSE_DIR=$COMPOSE_DIR
+EOF
+
+info "fine installazione. BUON DIVERTIMENTO"
+info "E' stato installato Home Assistant Supervised versione ${HASSIO_VERSION}"
+info "Puoi trovare la tua installazione a http://${IP_ADDRESS}:8123"
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+TIMEOUT=1
 echo "
 	 ##         ######   ###   ##  ##    ##  ##    ## 
 	 ##         ######   ###   ##  ##    ##  :##  ##: 
@@ -12,17 +13,40 @@ echo "
 	 ##           ##     ##  :###  ##    ##   ##::##  
 	 ########   ######   ##   ###  :######:  :##  ##: 
 	 ########   ######   ##   ###   :####:   ##    ## "
-sleep 3	  
+sleep $TIMEOUT	  
 echo "Benvenuto,figlio della perdizione "
 printf "\U$(printf %08x 128520)\n"
-sleep 3
+sleep $TIMEOUT
 echo "rimozione di home assistant!"
-sleep 3
+sleep $TIMEOUT
 echo "cominciamo!"
 
-WD=/opt/hassio
-cd $WD/setup
-docker-compose down --remove-orphans
+if [[ -f "/root/.ha_uninstall" ]]; then
+	. /root/.ha_uninstall
+else 
+	echo -n "In quale percorso è installato HA supervised?"
+	read BASE_DIR;
+fi
+if [[ ! -f "$COMPOSE_FILE" || -d "$COMPOSE_DIR" ]]; then
+	# il parametro COMPOSE_FILE e/o COMPOSE_DIR non esistono o non sono file/dir
+	# cerco il compose file nella directory di installazione
+	if [[ -f "$BASE_DIR/docker-compose.yml" ]]; then
+	  COMPOSE_FILE=$BASE_DIR/docker-compose.yml
+	fi
+
+	if [[ -f "$BASE_DIR/docker-compose.yaml" ]]; then
+	  COMPOSE_FILE=$BASE_DIR/docker-compose.yaml
+	fi
+	if [[ ! -f "$COMPOSE_FILE" ]]; then
+	  #non esiste nessuna delle due versioni del compose file pertanto lo chiediamo
+	  echo "Non trovo il file docker-compose.yaml nella cartella $BASE_DIR"
+	  echo -n "Mi sai indicare il percorso del docker-compose? "
+	  read COMPOSE_FILE;
+	fi
+fi
+cd $BASE_DIR
+
+docker-compose -f $COMPOSE_FILE down --remove-orphans
 
 docker stop hassio_audio
 docker stop hassio_cli
@@ -38,9 +62,15 @@ docker rm hassio_multicast
 docker rm hassio_observer
 docker rm homeassistant
 
+ADDONS=$(docker ps -f name=addon_core -q)
+for addon in ${ADDONS}; do 
+	docker stop $addon
+	docker rm $addon
+done;
 
 
-rm -rf /opt/hassio
+rm -rf $BASE_DIR
+rm -rf /root/.ha_uninstall
 
 sleep 3                                                                        
 
